0665135b6c89219cbd5b500e5430582caffcf211
Raj <rajp43447@gmail.com>
2023-10-01 00:54:50 +0530

    Cucumber Framework

---
+++ src/test/java/Qa/testcases/LoginPage.java
@@ -0,0 +1,12 @@
+package Qa.testcases;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.decorators.WebDriverDecorator;
+import org.testng.annotations.Test;
+public class LoginPage {
+
+	@Test
+	public void verifyLoginWithValidCredentials() {
+	WebDriver driver = new ChromeDriver();
+	}
+}
